69da6007422bff6405a2896d7c3d90ba02a78bee
Logan Phillips <logan.phillips1@students.snow.edu>
2026-06-17 20:14:35 -0600

    starting

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello! I will ask for words and make a short story.");
+Console.WriteLine();
+string originalStory ="I went to a (place) with my (adjective) friend. We saw a (noun) and played (game).";
+string[] parts = originalStory.Split(' ');
+ string newStory = "";
+
+

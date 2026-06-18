f1b8314ce47be7098950144a8aab9bb167f2db55
Logan Phillips <logan.phillips1@students.snow.edu>
2026-06-17 20:15:26 -0600

    going

--- Program.cs
+++ Program.cs
@@ -3,5 +3,12 @@ Console.WriteLine();
 string originalStory ="I went to a (place) with my (adjective) friend. We saw a (noun) and played (game).";
 string[] parts = originalStory.Split(' ');
  string newStory = "";
+for (int i = 0; i < parts.Length; i++)
+{
+string token = parts[i];
+int a = token.IndexOf('(');
+int b = token.IndexOf(')');
+if (a >= 0 && b > a)
+{
 
 
